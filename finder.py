1c7e2b536e5be2f6fb4b22911c6f395afbd58a24
Marat Ramazanov <sadzax@gmail.com>
2022-11-17 15:09:42 +0300

    3.0

--- finder.py
+++ finder.py
@@ -28,42 +28,46 @@ def finder(req):
             break
         else:
             i += 1
-            
-    song_url = song_url[62:]
-    song_url = song_url[:(len(song_url)-2)]
-    song_url = 'https://musicstax.com' + song_url
-
-    song_response = session.get(song_url).text.splitlines()
-
-    j = 0
-    while j < len(song_response):
-        if song_response[j].startswith('<div class="song-meta-title">'):
-            info = song_response[j+3]
-            break
-        else:
-            j += 1
     
-    def trimmer(dump, phrase, starter, stopper):
-        dump = dump[(dump.find(phrase))+len(phrase):]
-        if starter is None:
-            dump = dump[:dump.find(stopper)]
-        else:
-            dump = dump[(dump.find(starter))+len(starter):dump.find(stopper)]
-        return dump
+    try:
+        song_url = song_url[62:]
+        song_url = song_url[:(len(song_url)-2)]
+        song_url = 'https://musicstax.com' + song_url
 
-    key = trimmer(info,'data-cy="meta-Key-value">',None,'</div>')
-    tempo = trimmer(info,'data-cy="meta-Tempo-value">',None,'</div>')
-    song_name = trimmer(info,'data-cy="meta-Name-value">',None,'</div>')
-    artist_name = trimmer(info,'data-cy="meta-Artist(s)-value"><','<u>','</u>')
-    album_name = trimmer(info,'data-cy="meta-Album-value"><','<u>','</u>')
-    release_date = trimmer(info,'data-cy="meta-Release+Date-value">',None,'</div>')
+        song_response = session.get(song_url).text.splitlines()
 
-    answer = f'I guess you mean "{song_name}"\n'\
-            f'by {artist_name}\n'\
-            f'from the album "{album_name}" released on {release_date}\n\n'\
-            f'-- -- -- -- -- -- -- -- --\n\n'\
-            f'The song is probably in \n {key} \n\nTempo:\n {tempo} BPM'
-    return answer
+        j = 0
+        while j < len(song_response):
+            if song_response[j].startswith('<div class="song-meta-title">'):
+                info = song_response[j+3]
+                break
+            else:
+                j += 1
+        
+        def trimmer(dump, phrase, starter, stopper):
+            dump = dump[(dump.find(phrase))+len(phrase):]
+            if starter is None:
+                dump = dump[:dump.find(stopper)]
+            else:
+                dump = dump[(dump.find(starter))+len(starter):dump.find(stopper)]
+            return dump
+
+        key = trimmer(info,'data-cy="meta-Key-value">',None,'</div>')
+        tempo = trimmer(info,'data-cy="meta-Tempo-value">',None,'</div>')
+        song_name = trimmer(info,'data-cy="meta-Name-value">',None,'</div>')
+        artist_name = trimmer(info,'data-cy="meta-Artist(s)-value"><','<u>','</u>')
+        album_name = trimmer(info,'data-cy="meta-Album-value"><','<u>','</u>')
+        release_date = trimmer(info,'data-cy="meta-Release+Date-value">',None,'</div>')
+
+        answer = f'I guess you mean <b>"{song_name}</b>"\n'\
+                f'by <b>{artist_name}</b>\n'\
+                f'from the album "{album_name}" released on {release_date}\n\n'\
+                f'-- -- -- -- -- -- -- -- --\n\n'\
+                f'The song is probably in \n <u><b>{key}<b></u> \n\nTempo:\n <u><b>{tempo}</b></u> BPM'
+        return answer
+    except:
+        answer = f"Sorry, I didn't find any song. Please specify your request"
+        return answer
 
 tokenTG = io.open('token.txt', mode="r", encoding='utf-8').read()
 bot = telebot.TeleBot(tokenTG)
@@ -75,7 +79,7 @@ def start(message):
                             f" Just send me the name of the song and (preferably) it's perfomer \n\n\n\n" \
                             f'<b>Привет, {message.from_user.first_name}</b>\n\n' \
                             f' Я умею искать тональность и темп почти любой (зарубежной) песни \n' \
-                            f' Просто отправь мне её название и (очень желательно) исполнителя '
+                            f' Просто отправь мне её название и (чтобы было точнее) исполнителя '
     bot.send_message(message.chat.id, bot_resopnse_on_start, parse_mode='html')
 
 @bot.message_handler()
@@ -84,9 +88,9 @@ def main(message):
     if message.text == 'testme':
         bot.reply_to(message, f'<b>Your Technical Data:</b>\n\n{message}', parse_mode='html')
     elif message.text.lower() in three_hundred_list:
-        bot.reply_to(message, f'<b>ОТСОСИ У ТРАКТОРИСТА АХАХХААХХАХА))))))</b>')
+        bot.reply_to(message, f'<b>ОТСОСИ У ТРАКТОРИСТА АХАХХААХХАХА))))))</b>\n', parse_mode='html')
     else:
         find_this = message.text
-        bot.reply_to(message, finder(find_this))
+        bot.reply_to(message, finder(find_this), parse_mode='html')
 
 bot.polling(none_stop=True)
